14dc004c1c2493e2d9fc09f06901a2c67571f59b
Anodii <matheuslge@outlook.com>
2025-07-09 14:52:33 -0300

    Roleta teste 7

--- pages/api/roleta.js
+++ pages/api/roleta.js
@@ -1,14 +1,15 @@
 export default function handler(req, res) {
+  const sender = req.query.sender || "usuario";
   const x = Math.floor(Math.random() * 100);
 
   const resultado =
     x < 75
-      ? "$(sender) Nada aconteceu, tenta denovo aí Ta"
+      ? `${sender}, nada aconteceu. Tenta de novo ai Ta?`
       : x < 88
-      ? "/timeout $(sender.name) 300 Timeout de 5min pra ficar esperto kkkk"
+      ? `/timeout ${sender} 300 Timeout de 5min pra ficar esperto kkkk`
       : x < 99
-      ? "/timeout $(sender.name) 600 Timeout de 10min pra ficar esperto kkkk"
-      : "/ban $(sender.name) Toma ban, que sorte HAH kkkk ";
+      ? `/timeout ${sender} 600 Timeout de 10min pra ficar esperto kkkk`
+      : `/ban ${sender} Toma ban, que sorte HAH kkkk`;
 
   res.status(200).send(resultado);
 }
