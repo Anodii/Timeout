674db93d0054eb9a7041876ae01729d9dfa80a58
Anodii <matheuslge@outlook.com>
2025-07-15 13:11:03 -0300

    Roleta teste 14: adicionando grompinho como VIP

--- pages/api/roleta.js
+++ pages/api/roleta.js
@@ -8,13 +8,13 @@ export default function handler(req, res) {
     x < 66
       ? `${sender} Nada aconteceu, vai tentar de novo ai? Ta`
       : x < 86
-      ? `/timeout ${sender} 1 Timeout de 5min pra ficar esperto kkkk`
+      ? `/timeout ${sender} 300 Timeout de 5min pra ficar esperto kkkk`
       : x < 99.5
-      ? `/timeout ${sender} 4 Timeout de 10min pra ficar esperto kkkk`
+      ? `/timeout ${sender} 600 Timeout de 10min pra ficar esperto kkkk`
       : `/ban ${sender} Toma perma ban, que sorte HAH kkkk`;
   
-  if(sender == "namiss2" && x < 99.5){
-    resultado = `/timeout ${sender} 4 hi ${sender} kkkk rsrsrs`;
+  if(sender == "grompinho" && x < 99.5){
+    resultado = `/timeout ${sender} 300 hi ${sender} kkkk rsrsrs`;
   }
 
   res.status(200).send(resultado);
