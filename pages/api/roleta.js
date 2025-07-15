aa483bd739520ea7119208853bb3fb17d0256b1f
Anodii <matheuslge@outlook.com>
2025-07-15 11:55:25 -0300

    Roleta teste 11: aumentando chance de TO

--- pages/api/roleta.js
+++ pages/api/roleta.js
@@ -3,13 +3,13 @@ export default function handler(req, res) {
   const x = Math.random() * 100;
 
   const resultado =
-    x < 75
-      ? `${sender}, nada aconteceu. Tenta de novo ai Ta ${x.toFixed(2)}`
-      : x < 88
-      ? `/timeout ${sender} 1 Timeout de 5min pra ficar esperto kkkk ${x.toFixed(2)}`
+    x < 66
+      ? `${sender}, nada aconteceu. Tenta de novo ai Ta ${x.toFixed(1)}`
+      : x < 86
+      ? `/timeout ${sender} 1 Timeout de 5min pra ficar esperto kkkk ${x.toFixed(1)}`
       : x < 99.5
-      ? `/timeout ${sender} 4 Timeout de 10min pra ficar esperto kkkk ${x.toFixed(2)}`
-      : `/ban ${sender} Toma ban, que sorte HAH kkkk ${x.toFixed(2)}`;
+      ? `/timeout ${sender} 4 Timeout de 10min pra ficar esperto kkkk ${x.toFixed(1)}`
+      : `/ban ${sender} Toma ban, que sorte HAH kkkk ${x.toFixed(1)}`;
 
   res.status(200).send(resultado);
 }
