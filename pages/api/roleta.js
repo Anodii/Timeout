86df44e26361cc351a7d8fcf3aa7da7329b4729c
Anodii <matheuslge@outlook.com>
2025-07-15 12:21:43 -0300

    Roleta teste 12: adicionando VIP do TO

--- pages/api/roleta.js
+++ pages/api/roleta.js
@@ -2,14 +2,21 @@ export default function handler(req, res) {
   const sender = req.query.sender || "usuario";
   const x = Math.random() * 100;
 
-  const resultado =
+  let resultado;
+  
+  if(sender == "namiss2"){
+    resultado = `/timeout ${sender} 4 hi ${sender} kkkk`;
+  } else {
+
+  resultado =
     x < 66
-      ? `${sender}, nada aconteceu. Tenta de novo ai Ta ${x.toFixed(1)}`
+      ? `${sender} nada aconteceu. Tenta de novo ai Ta ${x.toFixed(1)}`
       : x < 86
       ? `/timeout ${sender} 1 Timeout de 5min pra ficar esperto kkkk ${x.toFixed(1)}`
       : x < 99.5
       ? `/timeout ${sender} 4 Timeout de 10min pra ficar esperto kkkk ${x.toFixed(1)}`
       : `/ban ${sender} Toma ban, que sorte HAH kkkk ${x.toFixed(1)}`;
-
+  }
+  
   res.status(200).send(resultado);
 }
