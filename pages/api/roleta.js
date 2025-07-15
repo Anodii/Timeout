a39344a3e9eced34ea76a57555f9ee414acdb94c
Anodii <matheuslge@outlook.com>
2025-07-15 11:16:47 -0300

    Roleta teste 10: diminuindo chance de ban

--- pages/api/roleta.js
+++ pages/api/roleta.js
@@ -1,15 +1,15 @@
 export default function handler(req, res) {
   const sender = req.query.sender || "usuario";
-  const x = Math.floor(Math.random() * 100);
+  const x = Math.random() * 100;
 
   const resultado =
     x < 75
-      ? `${sender}, nada aconteceu. Tenta de novo ai Ta ${x}`
+      ? `${sender}, nada aconteceu. Tenta de novo ai Ta ${x.toFixed(2)}`
       : x < 88
-      ? `/timeout ${sender} 300 Timeout de 5min pra ficar esperto kkkk ${x}`
-      : x < 99
-      ? `/timeout ${sender} 600 Timeout de 10min pra ficar esperto kkkk ${x}`
-      : `/ban ${sender} Toma ban, que sorte HAH kkkk ${x}`;
+      ? `/timeout ${sender} 1 Timeout de 5min pra ficar esperto kkkk ${x.toFixed(2)}`
+      : x < 99.5
+      ? `/timeout ${sender} 4 Timeout de 10min pra ficar esperto kkkk ${x.toFixed(2)}`
+      : `/ban ${sender} Toma ban, que sorte HAH kkkk ${x.toFixed(2)}`;
 
   res.status(200).send(resultado);
 }
