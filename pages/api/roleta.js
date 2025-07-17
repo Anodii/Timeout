90ab2327eb3d635346a1e57985324d581fe221e8
Anodii <matheuslge@outlook.com>
2025-07-17 16:15:13 -0300

    Roleta teste 16: ban = 0.1% e lukasdelta como vip

--- pages/api/roleta.js
+++ pages/api/roleta.js
@@ -9,11 +9,11 @@ export default function handler(req, res) {
       ? `${sender} Nada aconteceu, vai tentar de novo ai? Ta`
       : x < 86
       ? `/timeout ${sender} 300 Timeout de 5min pra ficar esperto kkkk`
-      : x < 99
+      : x < 99.9
       ? `/timeout ${sender} 600 Timeout de 10min pra ficar esperto kkkk`
       : `/ban ${sender} Toma perma ban, que sorte HAH kkkk`;
   
-  if(sender == "grompinho" && x < 99){
+  if((sender == "grompinho" || sender == "lukasdelta_") && x < 99.9){
     resultado = `/timeout ${sender} 300 hi ${sender} kkkk rsrsrs`;
   }
 
